579eeaa96424ecddf01675fb55f9de667e0f95dc
Ian Agpawa <agpawaji@gmail.com>
2017-01-03 22:22:31 -0500

    refactor: filter list, need to filter markers

--- js/app.js
+++ js/app.js
@@ -18,7 +18,37 @@ var ViewModel = function(){
 
     this.currentRestaurant = ko.observable( self.restaurantList[0] );
 
+    this.filter = ko.observable();
+
+    this.filteredList = ko.computed(function(){
+        var filter = self.filter();
+        if (!filter){
+            return self.restaurantList();
+        } else {
+
+            function lowerCased(input){
+                var arr = input.split(" ");
+                var newArr = [];
+
+                arr.forEach(function(word){
+                    newArr.push(word.toLowerCase());
+                })
+
+                return newArr.join(" ")
+            }
+
+            filter = lowerCased(filter);
+
+            return ko.utils.arrayFilter(self.restaurantList(),
+                function(restaurant){
+                    var lowerCase = lowerCased(restaurant.name());
+                    console.log(lowerCase.startsWith(filter))
+                    return lowerCase.startsWith(filter);
+                })
+        }
+    })
 }
+
 
 
 
