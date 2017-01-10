5c36463d6b0263e73169d6a8923d106beebf1f55
Ian Agpawa <agpawaji@gmail.com>
2017-01-09 22:41:18 -0500

    refactor: Set environmental variables

--- main.py
+++ main.py
@@ -52,7 +52,10 @@ class Handler(webapp2.RequestHandler):
 
 
     def get(self):
-        self.render("index.html")
+        client_id = os.environ.get("CLIENT_ID")
+        client_secret = os.environ.get("CLIENT_SECRET")
+        self.render("index.html", client_id = client_id,
+                    client_secret = client_secret)
 
 
 
